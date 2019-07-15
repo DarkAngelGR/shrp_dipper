22ecbdf8d347f32fa38f73282e650818e152db82
Darth9 <reyprimerjefe@gmail.com>
2019-07-15 23:20:08 +0100

    Fox-ify and port to dipper

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,6 +1,8 @@
 #
 # Copyright 2018 The Android Open Source Project
 #
+# Copyright (C) 2019-2020 OrangeFox Recovery Project
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -23,6 +25,8 @@
 # *not* include it on all devices, so it is safe even with hardware-specific
 # components.
 
+LOCAL_PATH := device/xiaomi/dipper
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
@@ -31,7 +35,7 @@ TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := kryo
 
 TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_ARCH_VARIANT := armv8-a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a9
@@ -50,7 +54,22 @@ BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET     := 0x01000000
-TARGET_PREBUILT_KERNEL := device/xiaomi/polaris/Image.gz-dtb
+NEED_KERNEL_MODULE_SYSTEM := true
+TARGET_KERNEL_ARCH := arm64
+
+ifeq ($(FOX_BUILD_FULL_KERNEL_SOURCES),1)
+  TARGET_KERNEL_SOURCE := kernel/xiaomi/dipper
+  TARGET_KERNEL_CONFIG := dipper-perf_defconfig
+  BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+  TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+else # FOX_BUILD_FULL_KERNEL_SOURCES==1
+   TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/prebuilt/Image.gz-dtb
+ifeq ($(FOX_USE_STOCK_KERNEL),1)
+   TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/prebuilt/Image-stock.gz-dtb
+endif    
+  PRODUCT_COPY_FILES += \
+    $(TARGET_PREBUILT_KERNEL):kernel
+endif  # FOX_BUILD_FULL_KERNEL_SOURCES==1
 
 # Platform
 TARGET_BOARD_PLATFORM := sdm845
@@ -75,17 +94,13 @@ TARGET_USERIMAGES_USE_F2FS := true
 TARGET_COPY_OUT_VENDOR := vendor
 
 # Recovery
-TARGET_OTA_ASSERT_DEVICE := polaris
+TARGET_OTA_ASSERT_DEVICE := dipper
 BOARD_HAS_LARGE_FILESYSTEM := true
 BOARD_HAS_NO_SELECT_BUTTON := true
 
 # Android Verified Boot
 BOARD_AVB_ENABLE := false
 BOARD_BUILD_DISABLED_VBMETAIMAGE := true
-
-# Crypto
-TW_INCLUDE_CRYPTO := true
-TW_INCLUDE_FBE := true
 
 # TWRP specific build flags
 TW_THEME := portrait_hdpi
@@ -94,15 +109,29 @@ BOARD_HAS_NO_REAL_SDCARD := true
 TARGET_RECOVERY_QCOM_RTC_FIX := true
 TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
 TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel0-backlight/brightness"
-TW_MAX_BRIGHTNESS := 4095
-TW_DEFAULT_BRIGHTNESS := 1500
+TW_MAX_BRIGHTNESS := 1023
+TW_DEFAULT_BRIGHTNESS := 420
 TW_INCLUDE_NTFS_3G := true
 TW_EXCLUDE_SUPERSU := true
 TW_EXTRA_LANGUAGES := true
+TW_DEFAULT_LANGUAGE := en
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_EXCLUDE_DEFAULT_USB_INIT := true
 TWRP_INCLUDE_LOGCAT := true
 TARGET_USES_LOGD := true
 PLATFORM_SECURITY_PATCH := 2099-12-31
-TW_USE_TOOLBOX := true
-TW_HAS_EDL_MODE := true
+TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/recovery.fstab
+AB_OTA_UPDATER := false
+
+# exFAT FS Support
+TW_INCLUDE_FUSE_EXFAT := true
+
+# NTFS Support
+TW_INCLUDE_FUSE_NTFS := true
+
+# Crypto
+TW_INCLUDE_CRYPTO := true
+TW_INCLUDE_CRYPTO_FBE := true
+TW_INCLUDE_FBE := true
+TARGET_KEYMASTER_WAIT_FOR_QSEE := true
+#
