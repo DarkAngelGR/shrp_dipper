7f61446d7231b21aa4619ee4f600a8f2404cf71e
Darth9 <reyprimerjefe@gmail.com>
2020-01-20 16:47:13 +0000

    Update for R10.1

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -15,9 +15,18 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+# See <http://www.gnu.org/licenses/>.
+#
+# Please maintain this if you use this script or any part of it
+#
 FDEVICE="dipper"
 if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	export OF_USE_LEGACY_CRYPTO=1
+        export PLATFORM_VERSION="10.0.0"
+        export PLATFORM_SECURITY_PATCH="2099-12-31"
+	export OF_SCREEN_H=2248
+	export OF_STATUS_H=80
+	export TW_DEFAULT_LANGUAGE="en"
 	export FOX_DELETE_AROMAFM=1
 	export OF_CLASSIC_LEDS_FUNCTION=1
 	export OF_USE_MAGISKBOOT=1
@@ -26,12 +35,23 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
 	export OF_OTA_RES_DECRYPT=1
 	export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
-	export FOX_REPLACE_BUSYBOX_PS=1
 	export FOX_USE_BASH_SHELL=1
+	export FOX_ASH_IS_BASH=1
 	export FOX_USE_NANO_EDITOR=1
 	export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
-	export OF_SCREEN_H=2248
-	export OF_STATUS_H=80
-add_lunch_combo omni_dipper-eng
+	export OF_FORCE_MAGISKBOOT_BOOT_PATCH_MIUI=1
+	export FOX_REPLACE_BUSYBOX_PS=1
+	export OF_USE_NEW_MAGISKBOOT=1
+
+        export OF_KEEP_DM_VERITY="1"  # enabled for R10.1
+
+	# let's see what are our build VARs
+	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
+  	   export | grep "FOX" >> $FOX_BUILD_LOG_FILE
+  	   export | grep "OF_" >> $FOX_BUILD_LOG_FILE
+  	   export | grep "TW_" >> $FOX_BUILD_LOG_FILE
+   	   export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
+ 	fi
+	add_lunch_combo omni_"$FDEVICE"-eng
 fi
 #
